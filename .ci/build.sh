3ba1c1e70eff0e3741e72f229a962ef9ac04fd51
Anindya Chatterjee <anidotnet@gmail.com>
2017-08-03 13:48:25 +0530

    build script modification

--- .ci/build.sh
+++ .ci/build.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
 if [[ -z "${TRAVIS_TAG}" ]]; then
-    ./gradlew -g /cache/.gradle clean build run asciidoc -Dscan -PnitriteVersion=$NITRITE_VERSION --stacktrace --info
+    ./gradlew clean build run asciidoc -Dscan -PnitriteVersion=$NITRITE_VERSION --stacktrace --info
 else
     openssl aes-256-cbc -pass pass:$PGP_KEY_PASSWORD -in .ci/secring.gpg.enc -out ~/secring.gpg -d
-    ./gradlew -g /cache/.gradle clean build run asciidoc -Prelease -PnitriteVersion=$NITRITE_VERSION -Psigning.keyId=$PGP_KEY_ID -Psigning.password=$PGP_KEY_PASSWORD -Psigning.secretKeyRingFile=$PGP_KEY_FILE uploadArchives closeAndReleaseRepository :nitrite-explorer:shadowJar -Dscan --stacktrace --info
+    ./gradlew clean build run asciidoc -Prelease -PnitriteVersion=$NITRITE_VERSION -Psigning.keyId=$PGP_KEY_ID -Psigning.password=$PGP_KEY_PASSWORD -Psigning.secretKeyRingFile=$PGP_KEY_FILE uploadArchives closeAndReleaseRepository :nitrite-explorer:shadowJar -Dscan --stacktrace --info
 fi
