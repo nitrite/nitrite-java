944c043190aee35d8f41b8c4130a597042261ea0
Anindya Chatterjee <anidotnet@gmail.com>
2017-12-06 20:26:50 +0530

    android example sdk version upgrade

--- .ci/prepare-travis.sh
+++ .ci/prepare-travis.sh
@@ -5,15 +5,25 @@ sudo apt-get -qqy update
 sudo apt-get install -y zip
 
 ## Android
-wget https://dl.google.com/android/android-sdk_r24.4.1-linux.tgz
-tar zxvf android-sdk_r24.4.1-linux.tgz
+export ANDROID_COMPILE_SDK="25"
+export ANDROID_SDK_TOOLS_REV="3859397"                                              # "26.0.1"
+
+mkdir $HOME/.android                                                                # For sdkmanager configs
+echo 'count=0' > $HOME/.android/repositories.cfg                                    # Avoid warning
+wget --quiet --output-document=android-sdk.zip https://dl.google.com/android/repository/sdk-tools-linux-${ANDROID_SDK_TOOLS_REV}.zip
+unzip -qq android-sdk.zip -d android-sdk-linux
 sudo mv android-sdk-linux /opt/android-sdk
-sudo apt-get update
+
 export ANDROID_HOME="/opt/android-sdk"
+export PATH=$PATH:${ANDROID_HOME}/tools/bin:${ANDROID_HOME}/platform-tools/
+sudo apt-get update
 sudo dpkg --add-architecture i386
 sudo apt-get -qqy update
 sudo apt-get -qqy install libncurses5:i386 libstdc++6:i386 zlib1g:i386
-echo y | /opt/android-sdk/tools/android --silent update sdk --no-ui --all --filter platform-tools,tools,build-tools-24.0.0,android-24,extra-android-m2repository
+
+echo y | sdkmanager --update
+echo y | sdkmanager --licenses
+echo y | sdkmanager 'platforms;android-'${ANDROID_COMPILE_SDK}
 
 ## Mongo
 sudo service mongod start
