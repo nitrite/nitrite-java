5c922c5d5c488581fff666ae3205bb1b2f0e9169
Anindya Chatterjee <anidotnet@gmail.com>
2018-02-10 11:34:03 +0530

    build.sh corrected

--- .ci/build.sh
+++ .ci/build.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 if [[ -z "${TRAVIS_TAG}" ]]; then
-    ./gradlew clean build run asciidoc -PnitriteVersion=$NITRITE_VERSION -Psigning.keyId=$PGP_KEY_ID -Psigning.password=$PGP_KEY_PASSWORD -Psigning.secretKeyRingFile=$PGP_KEY_FILE uploadArchives --stacktrace
+    ./gradlew clean build asciidoc -PnitriteVersion=$NITRITE_VERSION -Psigning.keyId=$PGP_KEY_ID -Psigning.password=$PGP_KEY_PASSWORD -Psigning.secretKeyRingFile=$PGP_KEY_FILE uploadArchives --stacktrace
 else
-    ./gradlew clean build run asciidoc -Prelease -PnitriteVersion=$NITRITE_VERSION -Psigning.keyId=$PGP_KEY_ID -Psigning.password=$PGP_KEY_PASSWORD -Psigning.secretKeyRingFile=$PGP_KEY_FILE uploadArchives closeAndReleaseRepository :nitrite-explorer:shadowJar --stacktrace
+    ./gradlew clean build asciidoc -Prelease -PnitriteVersion=$NITRITE_VERSION -Psigning.keyId=$PGP_KEY_ID -Psigning.password=$PGP_KEY_PASSWORD -Psigning.secretKeyRingFile=$PGP_KEY_FILE uploadArchives closeAndReleaseRepository :nitrite-explorer:shadowJar --stacktrace
 fi
