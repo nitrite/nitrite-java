c3adb345f21ec895bcfde5695486640b3ac82ef5
Anindya Chatterjee <anidotnet@gmail.com>
2018-04-04 12:56:06 +0530

    fix for #64, dependency version upgrade

---
+++ potassium-nitrite/src/test/kotlin/org/dizitart/kno2/BackportJavaTimeTest.kt
@@ -0,0 +1,78 @@
+package org.dizitart.kno2
+
+import com.fasterxml.jackson.core.JsonGenerator
+import com.fasterxml.jackson.core.JsonParser
+import com.fasterxml.jackson.databind.*
+import com.fasterxml.jackson.databind.module.SimpleModule
+import com.fasterxml.jackson.module.kotlin.KotlinModule
+import org.dizitart.no2.IndexType
+import org.dizitart.no2.mapper.JacksonMapper
+import org.dizitart.no2.objects.Id
+import org.dizitart.no2.objects.Index
+import org.dizitart.no2.objects.Indices
+import org.junit.Test
+import org.threeten.bp.LocalDateTime
+import org.threeten.bp.ZoneOffset
+import java.nio.file.Files
+import java.nio.file.Paths
+import java.util.*
+
+/**
+ *
+ * @author Anindya Chatterjee
+ */
+class BackportJavaTimeTest {
+    private val dbPath = getRandomTempDbFile()
+
+    @Indices(value = [(Index(value = "time", type = IndexType.NonUnique))])
+    data class TestData (
+        @Id val id: String = UUID.randomUUID().toString(),
+        val time: LocalDateTime
+    )
+
+    class ThreeTenAbpModule : SimpleModule() {
+        override fun setupModule(context: SetupContext?) {
+            addDeserializer(LocalDateTime::class.java, object : JsonDeserializer<LocalDateTime>() {
+                override fun deserialize(p: JsonParser?, ctxt: DeserializationContext?): LocalDateTime? {
+                    val timeStamp = p?.longValue
+                    return if (timeStamp == -1L || timeStamp == null) null else {
+                        LocalDateTime.ofEpochSecond(timeStamp, 0, ZoneOffset.UTC)
+                    }
+                }
+            })
+
+            addSerializer(LocalDateTime::class.java, object : JsonSerializer<LocalDateTime>() {
+                override fun serialize(value: LocalDateTime?, gen: JsonGenerator?, serializers: SerializerProvider?) {
+                    if (value == null) {
+                        gen?.writeNull()
+                    } else {
+                        val timeStamp = value.toEpochSecond(ZoneOffset.UTC)
+                        gen?.writeNumber(timeStamp)
+                    }
+                }
+            })
+            super.setupModule(context)
+        }
+    }
+
+    @Test
+    fun testIssue59() {
+        val db = nitrite {
+            path = dbPath
+            nitriteMapper = object : JacksonMapper() {
+                init {
+                    objectMapper.registerModule(KotlinModule())
+                    objectMapper.registerModule(ThreeTenAbpModule())
+                    objectMapper.disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS)
+                }
+            }
+        }
+
+        val repo = db.getRepository<TestData>()
+        val testData = TestData(time = LocalDateTime.now())
+        repo.insert(testData)
+        println(repo.find().firstOrDefault())
+
+        Files.delete(Paths.get(dbPath))
+    }
+}
