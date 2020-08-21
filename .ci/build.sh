9fe75b2674b3d5c564a78d76e49b1961519d8fbd
Anindya Chatterjee <anidotnet@gmail.com>
2020-08-21 23:33:45 +0530

    travis cleanup and github action paused

--- .ci/build.sh
+++ .ci/build.sh
@@ -1,3 +1,7 @@
 #!/usr/bin/env bash
 
-./gradlew clean build
+if [[ -z "${TRAVIS_TAG}" ]]; then
+    ./gradlew clean build publish -PnitriteVersion="$NITRITE_VERSION" -Psigning.keyId="$PGP_KEY_ID" -Psigning.password="$PGP_KEY_PASSWORD" -Psigning.secretKeyRingFile="$PGP_KEY_FILE" --stacktrace
+else
+    ./gradlew clean build publish -Prelease -PnitriteVersion="$NITRITE_VERSION" -Psigning.keyId="$PGP_KEY_ID" -Psigning.password="$PGP_KEY_PASSWORD" -Psigning.secretKeyRingFile="$PGP_KEY_FILE" --stacktrace
+fi
