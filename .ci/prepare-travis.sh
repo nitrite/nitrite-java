4421f4f2ac45263ae925b8b70041da209c07685c
Anindya Chatterjee <anidotnet@gmail.com>
2020-08-21 19:52:27 +0530

    self-hosted runner added

--- .ci/prepare-travis.sh
+++ .ci/prepare-travis.sh
@@ -6,10 +6,10 @@ sudo apt-get install -y zip
 
 ## Android
 export ANDROID_COMPILE_SDK="25"
-export ANDROID_SDK_TOOLS_REV="3859397"                                              # "26.0.1"
+export ANDROID_SDK_TOOLS_REV="3859397"                                                  # "26.0.1"
 
-mkdir $HOME/.android                                                                # For sdkmanager configs
-echo 'count=0' > $HOME/.android/repositories.cfg                                    # Avoid warning
+mkdir "$HOME"/.android                                                                  # For sdkmanager configs
+echo 'count=0' > "$HOME"/.android/repositories.cfg                                      # Avoid warning
 wget --quiet --output-document=android-sdk.zip https://dl.google.com/android/repository/sdk-tools-linux-${ANDROID_SDK_TOOLS_REV}.zip
 unzip -qq android-sdk.zip -d android-sdk-linux
 sudo mv android-sdk-linux /opt/android-sdk
@@ -24,8 +24,3 @@ sudo apt-get -qqy install libncurses5:i386 libstdc++6:i386 zlib1g:i386
 echo y | sdkmanager --update
 echo y | sdkmanager --licenses
 echo y | sdkmanager 'platforms;android-'${ANDROID_COMPILE_SDK}
-
-## Mongo
-sudo service mongod start
-sudo service mongod status
-sudo mongo admin --eval "db.getSiblingDB('benchmark').createUser({user: 'bench', pwd: 'bench', roles: [{role: 'readWrite', db: 'benchmark'}, {role: 'dbAdmin', db: 'benchmark'}]})"
--- .ci/release.sh
+++ .ci/release.sh
@@ -4,20 +4,20 @@ if [[ -z "${TRAVIS_TAG}" ]]; then
     echo "Not a tagged release"
 else
     echo "Copying artifacts"
-    cp nitrite-datagate/build/libs/nitrite-datagate-$NITRITE_VERSION.jar nitrite-datagate/src/main/dist/lib/nitrite-datagate.jar
-    cp nitrite-datagate/build/libs/nitrite-datagate-$NITRITE_VERSION.jar nitrite-datagate/src/main/docker/nitrite-datagate.jar
+    cp nitrite-datagate/build/libs/nitrite-datagate-"$NITRITE_VERSION".jar nitrite-datagate/src/main/dist/lib/nitrite-datagate.jar
+    cp nitrite-datagate/build/libs/nitrite-datagate-"$NITRITE_VERSION".jar nitrite-datagate/src/main/docker/nitrite-datagate.jar
     rm nitrite-datagate/src/main/dist/lib/.gitkeep
     echo "Creating DataGate zip distribution"
-    cd nitrite-datagate/src/main/dist/
-    zip -r nitrite-datagate-$NITRITE_VERSION.zip bin conf lib
-    cd -
-    echo $(pwd)
+    cd nitrite-datagate/src/main/dist/ || exit
+    zip -r nitrite-datagate-"$NITRITE_VERSION".zip bin conf lib
+    cd - || exit
+    pwd
     echo "Creating docker image"
-    cd nitrite-datagate/src/main/docker
+    cd nitrite-datagate/src/main/docker || exit
     chmod +x datagate.sh
-    docker build -t dizitart/nitrite-datagate:$NITRITE_VERSION .
-    docker tag dizitart/nitrite-datagate:$NITRITE_VERSION dizitart/nitrite-datagate:latest
-    docker login -u $DOCKER_USER -p $DOCKER_PASS
+    docker build -t dizitart/nitrite-datagate:"$NITRITE_VERSION" .
+    docker tag dizitart/nitrite-datagate:"$NITRITE_VERSION" dizitart/nitrite-datagate:latest
+    docker login -u "$DOCKER_USER" -p "$DOCKER_PASS"
     echo "Publishing docker image"
     docker push dizitart/nitrite-datagate
 fi
